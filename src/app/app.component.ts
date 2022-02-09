f7fba657f222d404bff963bdbe1a0d8a89a129e3
DemeterCaubang <dpcaubang@student.apc.edu.ph>
2022-02-10 03:17:52 +0800

    custom icons embedded and finished home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Home', url: '/home', icon:'home' },
-    { title: 'Community', url: '/community', icon: 'chatbubbles' },
-    { title: 'Webinars', url: '/webinars', icon: 'videocam' },
-    { title: 'Health and Wellness Corner', url: '/health-and-wellness', icon: 'fitness' },
-    { title: 'Consultation', url: '/consultation', icon: 'calendar' },
-    { title: 'Quote of the Day', url: '/quote', icon: 'aperture' },
   ];
   public labels = ['FAQ', 'About'];
   constructor() {}
